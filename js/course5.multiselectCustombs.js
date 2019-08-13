f9ba6abf5ff2ff86bf363a3abe3b8bab625c958b
Dhananjay <dhananjay.sharma@cinestaan.com>
2019-08-13 12:21:14 +0530

    fonts added

--- js/course5.multiselectCustombs.js
+++ js/course5.multiselectCustombs.js
@@ -1,23 +1,30 @@
 $(function () {
-        $('#state, #food').multiselect({
-          includeSelectAllOption: true,
-          enableFiltering: true,
-          includeSaveOption: true,
-        });
-}); 
+    $('#state, #food').multiselect({
+        includeSelectAllOption: true,
+        enableFiltering: true,
+        includeSaveOption: true,
+    });
+
+    $('#timeslot').multiselect(
+            {
+                templates: {
+                    saveNew: '<li><a><label style="display:inline;"></label><input type="text" /></a></li>'
+                }
+            });
+});
 
 function getOnClickSave(id, selectedVals) {
     var _tmpValues = [];
 
-    $(selectedVals).each(function(index, row){
-       var _tmpVal = row.value;
-       _tmpValues.push(_tmpVal);
+    $(selectedVals).each(function (index, row) {
+        var _tmpVal = row.value;
+        _tmpValues.push(_tmpVal);
     });
 
     alert('Hello its done : ' + id + ' : ' + _tmpValues.join(','));
-} 
- 
+}
 
-function logOnConsole(msg){
+
+function logOnConsole(msg) {
     console.log(msg);
 }
